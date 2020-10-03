00475b0678d605ba34191ac6e998a1d454844d0e
Jorge Luis Ibarra Borbas <jl@Jorges-MacBook-Pro.local>
2020-10-03 15:18:12 -0700

    upload whatever react template for pages

---
+++ llorenc/src/SocialFollow.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faLinkedin,
+  faTwitter,
+  faGithub,
+  faMedium
+} from "@fortawesome/free-brands-svg-icons";
+
+export default function SocialFollow() {
+  return (
+    <div class="social-container">
+      <h3 className="heading">Created by</h3>
+      <a
+        href="https://www.linkedin.com/in/jorge-ibarra-borbas/"
+        className="linkedin social"
+      >
+        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+      </a>
+      <a href="https://github.com/chifffon" className="github social">
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
+      <a href="https://twitter.com/no" className="twitter social">
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+    </div>
+  );
+}
